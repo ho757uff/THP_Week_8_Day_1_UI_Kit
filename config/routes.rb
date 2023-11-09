72562547c85fa3455dfb5f93d9ada96ada531e72
ho757uff <robindebessel.pro@gmail.com>
2023-11-09 12:41:47 +0100

    Created : static pages + controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,21 @@
 Rails.application.routes.draw do
-  get "home/index"
-  # Utilise HomeController#index comme page d'accueil
+  # Devise routes with custom path names
+  devise_for :users, path: "", path_names: {sign_in: "login", sign_out: "logout"}
+
+  # Root route
+  root "home#index"
+
+  # Static pages
+  get "about", to: "static_pages#about"
+  get "contact", to: "static_pages#contact"
+
+  # Events routes using resources
+  resources :events, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+  # Attendances routes (you might want to use resources here as well)
   get "attendances/create"
   get "attendances/destroy"
-  get "events/index"
-  get "events/show"
-  get "events/new", to: "events#new", as: :new_event # Ajoutez cette ligne
-  get "events/edit"
-  get "events/create"
-  get "events/update"
-  get "events/destroy"
-  get "events" => "events#index", as: :events
 
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/") to the home controller's index action
-  root "home#index"
+  # Health check route
+  get "up" => "rails/health#show", :as => :rails_health_check
 end
