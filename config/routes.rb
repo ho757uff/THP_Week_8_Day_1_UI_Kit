339e532f02c3739bb97a0128543703d726d855ca
ho757uff <robindebessel.pro@gmail.com>
2023-11-09 12:27:33 +0100

    VIP - Login + Signout Working

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get "events/destroy"
   get "events" => "events#index", as: :events
 
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
